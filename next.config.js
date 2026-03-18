79535bbff7245de8142320373b34ceade9a62d33
CodeWithTheSaurabh <sourabhsharma7ss7@gmail.com>
2026-03-18 12:15:37 +0530

    fix: configure static export for Render Static Site

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  // Disable image optimization, as it requires a server
+  images: { unoptimized: true }
+}
 
 module.exports = nextConfig
